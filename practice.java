a413a52cd915480dca94b772d50592ce38ef0138
harujamu <tnagisa1220@gmail.com>
2023-01-14 13:48:33 +0000

    [Add]JAVA文法（文字列表示）、コンパイル、実行方法学習

--- practice.java
+++ practice.java
@@ -1,5 +1,12 @@
-public class Ready01{
+class Ready01{
 	public static void main(String [] args) {
-		System.out.println("Hello,world!");
+		System.out.println("文字を表示！JAVAの実行方法は、"
+		+ "①javac  ファイル名でコンパイル ②java  class名");
+	}
+}
+
+class Ready02{
+	public static void main(String [] args) {
+		System.out.println("こんにちは！");
 	}
 }
